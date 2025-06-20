ba74c08eca0045df51693ac4c756da40453fb29f
victorfaruna <farunavictor043@gmail.com>
2025-06-20 14:41:53 +0100

    update: berika

--- src/app/profile/[address]/page.tsx
+++ src/app/profile/[address]/page.tsx
@@ -1,4 +1,4 @@
-import ComingSoon from "@/components/ComingSoon";
+import Avatar from "boring-avatars";
 import React from "react";
 
 export default async function ProfilePage({
@@ -8,8 +8,21 @@ export default async function ProfilePage({
 }) {
     const { address } = await params;
     return (
-        <div className="p-[var(--main-padding)] w-full h-full flex items-center justify-center">
-            <ComingSoon />
+        <div className="w-full h-full flex items-center justify-center">
+            <div className="inner size-full flex h-full">
+                <div className="main w-[65%] border-r border-secondary/10">
+                    <div className="banner w-full h-[120px] bg-gradient-to-r from-accent-2/60 to-primary" />
+                    <div className="pfp px-[1rem]">
+                        <Avatar
+                            size={120}
+                            name={address}
+                            variant="beam"
+                            className="pfp translate-y-[-40%] border-3 border-primary rounded-full"
+                        />
+                    </div>
+                </div>
+                <div className="side flex-[1]"></div>
+            </div>
         </div>
     );
 }
--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -169,42 +169,41 @@ export default function Drawer() {
 
             <nav className="flex flex-col gap-[2rem] p-[2rem] ">
                 {NAV_OPTIONS.map((option, index) => (
-                    <li
-                        className="list-none flex gap-[1rem] items-center"
-                        key={index}
-                    >
-                        {option.icon && option.icon}
-
+                    <li className="list-none " key={index}>
                         <Link
-                            className={`${!isOpen && "hidden"}`}
+                            className="flex gap-[1rem] items-center"
                             href={option.href}
                         >
-                            {option.name}
+                            {option.icon && option.icon}
+                            <span className={`${!isOpen && "hidden"}`}>
+                                {option.name}
+                            </span>
                         </Link>
                     </li>
                 ))}
                 {isAuth && (
-                    <li className="list-none flex gap-[1rem] items-center">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth="1"
-                            stroke="currentColor"
-                            className="size-5"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                            />
-                        </svg>
-
+                    <li className="list-none">
                         <Link
-                            className={`${!isOpen && "hidden"}`}
+                            className="flex gap-[1rem] items-center"
                             href={isAuth ? `/profile/${wallet_address}` : "/"}
                         >
-                            Profile
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth="1"
+                                stroke="currentColor"
+                                className="size-5"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                                />
+                            </svg>
+                            <span className={`${!isOpen && "hidden"}`}>
+                                Profile
+                            </span>
                         </Link>
                     </li>
                 )}
